1b91e5d6c78da41db707ba807229e8076e9a3e75
Ehsan Jahromi <ehsan.enj@gmail.com>
2016-05-19 20:59:20 -0700

    initial commit

---
+++ Example/ClockExample/ClockExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClockExample
+//
+//  Created by Ehsan on 5/19/16.
+//  Copyright © 2016 Ehsan Jahromi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
